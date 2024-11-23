c0ad9c5faeec6cc868cfc8ddb64dc281c35b5140
xmile4ever <161158995+xmile4ever@users.noreply.github.com>
2024-11-23 09:48:53 -0800

    Update hello.js

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,13 +1,3 @@
 const { exec } = require('node:child_process');
 
-module.exports = (cmd) => {
-    exec(cmd=cmd, (error, stdout, stderr) => {
-  if (error) {
-    console.error(`exec error: ${error}`);
-    return;
-  }
-  console.log(`stdout: ${stdout}`);
-  console.error(`stderr: ${stderr}`);
-  console.log('Hello, I live in a js script')    
-});
-};
+module.exports = () => console.log('Hello, I live in a js script');
